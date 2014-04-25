d8dea4d9992f0a2b8c593270a0d79318f86f1327
Matt McGill <mmcgill@lonocloud.com>
2014-04-25 12:55:51 +0000

    add lonocloud private repo for internal deployment

--- project.clj
+++ project.clj
@@ -5,4 +5,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [lonocloud/riemann-java-client "0.2.9"]])
+                 [lonocloud/riemann-java-client "0.2.9"]]
+  :plugins [[s3-wagon-private "1.1.2"]]
+  :repositories [["lonocloud-releases" {:url "s3://lonocloud-maven/repo/releases/"
+                                        :username :env
+                                        :passphrase :env
+                                        :sign-releases false}
+                  "lonocloud-snapshots" {:url "s3://lonocloud-maven/repo/snapshots/"
+                                         :username :env
+                                         :passphrase :env
+                                         :sign-releases false}]])
